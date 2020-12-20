d0b8015d0e6402e12e8d8c716a8b157ba13ab8d5
Victor Nascimento Juliano Gomes <victor.nnjg@gmail.com>
2020-12-20 13:48:19 -0300

    retryWhen and delayWhen

--- server/get-courses.route.ts
+++ server/get-courses.route.ts
@@ -7,23 +7,21 @@ import { COURSES } from "./db-data";
 
 export function getAllCourses(req: Request, res: Response) {
 
-  /*
   const error = (Math.random() >= 0.5);
 
   if (error) {
-      console.log("ERROR loading courses!");
-      res.status(500).json({message: 'random error occurred.'});
+    console.log("ERROR loading courses!");
+    res.status(500).json({ message: 'random error occurred.' });
   }
   else {
-  */
 
-  setTimeout(() => {
+    setTimeout(() => {
 
-    res.status(500).json({ message: 'random error occurred.' });
+      res.status(200).json({ payload: Object.values(COURSES) });
 
-  }, 200);
+    }, 200);
 
-  // }
+  }
 }
 
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of, throwError } from 'rxjs';
-import { catchError, finalize, map, shareReplay, tap } from 'rxjs/operators';
+import { Observable, timer } from 'rxjs';
+import { delayWhen, finalize, map, retryWhen, shareReplay, tap } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
 import { Course } from "../model/course";
 
@@ -20,23 +20,6 @@ export class HomeComponent implements OnInit {
     const http$ = createHttpObservable('/api/courses');
 
     const courses$ = http$.pipe(
-      catchError((err) => {
-        /* RECOVERERY LOGIC */
-        // return of({
-        //   payload: [{
-        //     id: 0,
-        //     description: "RxJs In Practice Course",
-        //     iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
-        //     courseListIcon: 'https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png',
-        //     longDescription: "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
-        //     category: 'BEGINNER',
-        //     lessonsCount: 10
-        //   }]
-        // });
-
-        /* RETHROW ERROR LOGIC */
-        return throwError(err);
-      }),
       finalize(() => console.log('finalize executed')),
       tap(() => console.log('http request executed!')),
       map((res) => {
@@ -47,6 +30,9 @@ export class HomeComponent implements OnInit {
         }
       }),
       shareReplay(),
+      retryWhen((errors) => errors.pipe(
+        delayWhen(() => timer(2000))
+      )),
     );
 
     this.beginnerCourses$ = courses$.pipe(
